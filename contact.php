28bce47e3e23211d1aeefe1ec94e51d77b4a9c1b
pratha17 <thakur.udaipur@gmail.com>
2018-06-20 10:45:06 +0530

    added contact us page

--- contact.php
+++ contact.php
@@ -2,7 +2,7 @@
 
 	<div class="intro intro-small">
 		<div class="intro-image fullsize-image-container" data-stellar-background-ratio="0.54">
-			<img src="assets/images/temp/intro3.jpg" class="fullsize-image" alt="" />
+			<img src="assets/images/s1.jpg" class="fullsize-image" alt="" />
 		</div><!-- /.intro-image -->
 
 		<div class="row">
@@ -13,6 +13,76 @@
 			</div><!-- /.intro-caption -->
 		</div><!-- /.row -->
 	</div><!-- /.intro intro-small -->
+
+	<div class="main">
+		<div class="section-services section-doctors section-doctors-alt" itemscope itemtype="http://schema.org/Product">
+			<div class="tabs tabs-services">
+
+				<div class="row row-alt">
+					<p class="breadcrumbs">
+						<a href="#">Home</a>
+						
+						<span>
+							<i class="fa fa-angle-right"></i>
+						</span>
+					
+						<a href="#" class="current">Contact us</a>
+					</p><!-- /.breadcrumbs -->
+				</div><!-- /.row -->
+
+				<div class="tabs-body">
+					<div class="tab-body current" id="tab1">
+						<div class="row">
+							<div class="columns large-12 medium-12">
+								<iframe src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14513.560374093317!2d73.7280898!3d24.5757301!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x9200c3904c7caaea!2sNetCoders!5e0!3m2!1sen!2sin!4v1529470912852" width="100%" height="300" frameborder="0" style="border:0" allowfullscreen></iframe>
+							</div><!-- /.columns large-12 -->
+							<div class="columns medium-12 large-12">
+						
+								<div class="columns medium-6">
+									<h3>Montreal</h3>
+
+									<p>
+										51 Sherbrooke West <br>
+										Montréal (QC), H4B 2Y2
+									</p>
+									
+									<p>
+										Office &mdash; (514) 888-8888 <br>
+										Fax &mdash; (514) 888-9999
+									</p>
+									
+									<p>
+										Email: <a href="#">office@constuction.com</a><br>
+										web: <a href="#">http://construction.com</a>
+									</p>
+								</div><!-- /.columns medium-6 -->
+						
+								<div class="columns medium-6">
+									<h3>New York</h3>
+
+									<p>
+										51 Sherbrooke West <br>
+										New York, 45678
+									</p>
+									
+									<p>
+										Office &mdash; +1 (514) 000-0000<br>
+										Fax &mdash; +1 (514) 000-0000
+									</p>
+									
+									<p>
+										Email: <a href="#">office@constuction.com</a><br>
+										web: <a href="#">http://construction.com/ny/</a>
+									</p>
+								</div><!-- /.columns medium-6 -->
+
+							</div><!-- /.columns medium-12 -->
+						</div><!-- /.row -->
+
+				</div><!-- /.tabs-body -->
+			</div><!-- /.tabs -->
+		</div>
+	</div>
 
 
 <?php include('include/footer.php'); ?>
